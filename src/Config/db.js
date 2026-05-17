f01c7ef35efa58e2649054da3a9b71a399d05892
EdoardoRocha <pazedoardo@gmail.com>
2026-05-16 23:06:00 -0300

    fix: add pg and pg-hstore package for sequelize

--- src/Config/db.js
+++ src/Config/db.js
@@ -1,20 +1,34 @@
 import { Sequelize } from "sequelize";
 
-const conn = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: console.log
-});
+const conn = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: "postgres",
+        logging: console.log,
+      },
+    );
 
 async function connection() {
-    try {
-        await conn.authenticate();
-        console.log("Conexão estabelecida com Postgres");
-    } catch (error) {
-        console.error(error);
-    };
-};
-
+  try {
+    await conn.authenticate();
+    console.log("Conexão estabelecida com Postgres");
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 connection();
 export default conn;
