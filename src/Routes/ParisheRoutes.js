5deb227cdff3f48170b1b72c4f9299d5ee007d34
EdoardoRocha <pazedoardo@gmail.com>
2026-05-29 12:26:09 -0300

    fix: free access for get all parishes

--- src/Routes/ParisheRoutes.js
+++ src/Routes/ParisheRoutes.js
@@ -10,8 +10,6 @@ import checkRole from "../Middlewares/authRole.js";
 
 router.get(
   "/",
-  checkToken,
-  checkRole(["Catequista", "Coordenador", "Admin"]),
   ParisheController.getAllParishes,
 );
 
